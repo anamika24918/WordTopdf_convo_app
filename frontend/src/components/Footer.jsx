ac8244ba393b853a0d51b5ccfd9dfa5d5dd700cb
anamika24918 <120499409+anamika24918@users.noreply.github.com>
2024-07-12 22:20:52 +0530

    Done !!!

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <>
+    <div>
+    <hr className=" border-black"/>
+        <h1 className="text-center py-3 text-sm">
+            Copyright @2024 - All right reserved by ACME Industries Ltd.
+        </h1>
+    </div>
+    </>
+  )
+}
